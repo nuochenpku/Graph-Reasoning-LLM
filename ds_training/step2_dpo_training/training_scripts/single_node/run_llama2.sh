e39abfc857ddd822a8041c6c47de03ba6e6b4951
Jerry Chen <96414623+nuochenpku@users.noreply.github.com>
2024-01-23 23:06:58 +0800

    Update run_llama2.sh

--- ds_training/step2_dpo_training/training_scripts/single_node/run_llama2.sh
+++ ds_training/step2_dpo_training/training_scripts/single_node/run_llama2.sh
@@ -7,35 +7,35 @@ OUTPUT=$1
 ZERO_STAGE=$2
 
 if [ "$OUTPUT" == "" ]; then
-    OUTPUT=/cpfs/user/chennuo/CN/output/dpo_math/ds_7b_v1_reason_dpo_sft/
+    OUTPUT=/cpfs/user/chennuo/CN/output/deepspeed/nlgreasoning/mistral_dpo_beta0.1/
 fi
 if [ "$ZERO_STAGE" == "" ]; then
     ZERO_STAGE=3
 fi
 mkdir -p $OUTPUT
 
-deepspeed --include localhost:0,1,2,3 --master_port=29592 main.py  \
-   --data_path local/jsonfile_verify/cpfs/user/chennuo/CN/gsm8k-ScRel/data/dpo/train_ins_only_rea_dpo.json \
+deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port=25001 main.py  \
+   --data_path local/jsonfile_graph/cpfs/user/chennuo/CN/Graph-Reasoning-LLM/datasets/data/dpo_datas/dpo_train_dsformat_V1.json \
    --data_split 0,10,0 \
-   --model_name_or_path /cpfs/user/chennuo/CN/output/dpo_math/ds_7b_rea_sft \
+   --model_name_or_path /cpfs/user/chennuo/CN/output/deepspeed/nlgreasoning/mistral_rft_k5_new/ \
    --per_device_train_batch_size 2 \
    --per_device_eval_batch_size 2 \
-   --max_seq_len 768 \
-   --learning_rate 2e-5  \
+   --max_seq_len 2048 \
+   --learning_rate 5e-6  \
    --weight_decay 0. \
-   --num_train_epochs 2  \
-   --beta 0.01 \
+   --num_train_epochs 3  \
    --gradient_accumulation_steps 2 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 100 \
    --seed 1234 \
+   --beta 0.1 \
+    --print_loss \
    --zero_stage $ZERO_STAGE \
    --deepspeed \
-   --print_loss \
-   --add_sft \
+   --data_output_path $OUTPUT \
    --gradient_checkpointing \
    --output_dir $OUTPUT \
-   &> $OUTPUT/training.log &
+#    &> $OUTPUT/training.log &
 
 #    9.65e-6
 #    --gradient_checkpointing \
