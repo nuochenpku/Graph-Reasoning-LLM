35f2c0596b3efe7749bda3efe37d9fb478388bfa
Jerry Chen <96414623+nuochenpku@users.noreply.github.com>
2024-02-19 21:07:26 +0800

    Update rft.sh

--- evaluation/rft.sh
+++ evaluation/rft.sh
@@ -1,16 +1,7 @@
 
-export MODEL_PATH='/cpfs/user/chennuo/CN/output/graph_nlg/7b_v2'
-
-
-for seed in $(seq 1 100)
-do
-    echo $seed
-    mkdir -p $MODEL_PATH/$seed
-    bash test_graph.sh $seed
-    # CUDA_VISIBLE_DEVICES=4,5,6,7 python3  rft_llama.py --model_path $MODEL_PATH \
-    # --streategy Parallel \
-    # --batch_size 8 \
-    # --seed $seed \
-    # &> $MODEL_PATH/$seed/math_graph.log &
-
-done
+export MODEL_PATH=$SFT_MODEL_PATH
+# mkdir -p $MODEL_PATH/$1
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3  rft_llama.py --model_path $MODEL_PATH \
+    --batch_size 16 \
+    --seed 20 \
+&> $MODEL_PATH/rft_graph_20.log &
